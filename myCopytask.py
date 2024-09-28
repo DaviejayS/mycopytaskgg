ee9f451b94fdaba95aad6ba52872b988654f0e5b
DaviejayS <victorino@gmail.com>
2024-09-28 11:19:36 +0800

    my copy task sir

---
+++ myCopytask.py
@@ -0,0 +1,7 @@
+age = [-12,13,-14,15,-16]
+
+for dataage in age:
+    if dataage > 0:
+        print (str(dataage)+ "negative")
+    elif dataage < 0:
+        print (str(dataage)+ "positive")
---
+++ myCopytask2.py
@@ -0,0 +1,8 @@
+Username = ["juan", "pedro", "james", "sagaad", "fuego"]
+
+for datauser in Username:
+    if datauser == "james":
+        print("james cute")
+    else:
+        print("james none")
+            
